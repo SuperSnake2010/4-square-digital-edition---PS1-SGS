1606c61baa50a130d4c0d39ff9eb5f79cb584d5d
SuperSnake2010 <83698542+SuperSnake2010@users.noreply.github.com>
2022-11-06 09:41:20 +0800

    Add files via upload

---
+++ HOME.c
@@ -0,0 +1,44 @@
+// set up the menu graphics.
+
+#include <graphics.h>
+int main()
+{
+    int gd = DETECT, gm;
+    initgraph (&gd, &gm, "");
+    int x = 0;
+    int y = 0;
+    int font = 10;
+    int direction = 0;
+    int font_size = 30;
+    settextstyle (font, direction, font_size);
+    outtextxy(x, y, "4-SQUARE");
+    getch();
+    closegraph();
+    return 0;
+}
+initgraph;
+
+void setfillstyle(int pattern, int color)
+void floodfill(int x, int y, int border_color)
+
+// draw the menu graphics.
+
+pattern = HATCH_FILL, Color = RED
+circle : x = 500, y = 500, radius = 200
+
+// make the text.
+
+void settextstyle(int font, int direction, int font_size);
+
+// check for inputs.
+
+while(1)
+{
+    // check for the controller input.
+
+    padCheck(Pad1Up):
+    ptr = fopen(\MENUS\GAME.c);
+
+}
+
+// end of main menu code.
